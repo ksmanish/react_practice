a092075c27dafdec2ba7f372faaaeb1c12b4efb6
ksmanish <lit2016006@iiitl.ac.in>
2019-12-11 12:39:27 +0530

    save

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saved to reloa converted to class changes made by me.
+          Edit <code>src/App.js</code> and saved to reloaded by converted to class changes made by me.
         </p>
         <a
           className="App-link"
