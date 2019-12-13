ec0772819d7681db9a703b5e799f4dd4c9fae922
ksmanish <lit2016006@iiitl.ac.in>
2019-12-13 13:26:31 +0530

    componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  } 
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}));
+  }
+
+  
   render(){
-    return (<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and saved to reloaded by converted to class changes made by me.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>)
+    return (
+    <div className="App">
+      {this.state.monsters.map(monster => (
+      <h1 key={monster.id}> {monster.name}</h1>
+      ))}
+    </div>
+    );
   }
 }
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and saved to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
+
+  
+
+
 
 export default App;
